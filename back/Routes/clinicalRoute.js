8edb0e842704a4e365ae70eedbd479a76b47495f
CorentinNrd <corentin.nordmann@epitech.eu>
2022-03-27 14:52:13 +0200

    create - récupération des informations de mon formulaire de recherche, vérification de ses informations avec les informations de mes essais clinique et envoie des objets triés et traités

--- back/Routes/clinicalRoute.js
+++ back/Routes/clinicalRoute.js
@@ -2,19 +2,36 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
-// const bcrypt = require("bcrypt");
-// const UserModel = require('../Model/UserModel')
 const excelToJson = require('convert-excel-to-json');
- 
+
 const result = excelToJson({
-    sourceFile: 'essai_clinique.xlsx'
+    sourceFile: 'essai_clinique.xlsx',
+    header: {
+        rows: 1
+    }
 });
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
 router.get("/", async (req, res, next) => {
     res.send(result.Sheet1)
-    // res.send({ message : "je suis la"})
+})
+
+router.post("/search", async (req, res, next) => {
+    let array = [];
+    result.Sheet1.map(element => {
+        if (req.body.search.specialite == element.B) {
+            array.push(element)
+            return element;
+        } 
+        if (req.body.search.organes === element.C) {
+            array.push(element)
+        }
+        if (req.body.search.situation == element.D) {
+            array.push(element)
+        }
+    })
+    res.send(array)
 })
 
 module.exports = router;
