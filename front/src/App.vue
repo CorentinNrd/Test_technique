ae0c5b792ee3b6d5f7fa4df542c3ee67806dfca9
CorentinNrd <corentin.nordmann@epitech.eu>
2022-03-28 15:19:50 +0200

    delete - suppression du footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <div>
     <Nav />
-    <Footer />
   </div>
 </template>
 
 <script>
 import Nav from "./components/Nav.vue";
-import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    Nav,
-    Footer,
+    Nav
   },
 };
 </script>
