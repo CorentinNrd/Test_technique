a8e91db8f23def8a717c3c8cde8ede25fe804cc4
CorentinNrd <corentin.nordmann@epitech.eu>
2022-03-28 10:42:18 +0200

    create - création du model pour enregister les essais clinique en favoris

---
+++ back/Model/EssaiModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const EssaiModel = new Schema({
+    ID: { type: String, required: true },
+    specialite: { type: String, required: true },
+    organes: { type: String, required: true },
+    situation: {type: String, required: true},
+}, { timestamps: true });
+
+var Essai = mongoose.model('Essai', EssaiModel);
+module.exports = Essai;
