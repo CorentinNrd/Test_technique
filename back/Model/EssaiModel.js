c5c63f054bccfaebbee5ff395ce4704460876425
CorentinNrd <corentin.nordmann@epitech.eu>
2022-03-28 10:50:48 +0200

    update - ajout de l'id de l'utilisateur

--- back/Model/EssaiModel.js
+++ back/Model/EssaiModel.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 
 const EssaiModel = new Schema({
     ID: { type: String, required: true },
+    id_user: { type: String, required: true },
     specialite: { type: String, required: true },
     organes: { type: String, required: true },
     situation: {type: String, required: true},
