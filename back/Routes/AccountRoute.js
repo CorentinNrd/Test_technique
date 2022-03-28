3d7918e38a98c229db433e97de4c7c1ea74c89bb
CorentinNrd <corentin.nordmann@epitech.eu>
2022-03-28 12:59:58 +0200

    create - création de la récupération des essais clinique de l'utilisateur et envoie des données récupérer au front

---
+++ back/Routes/AccountRoute.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const EssaiModel = require('../Model/EssaiModel')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+router.post("/", async (req, res, next) => {
+    let id_user = req.body.user;
+
+    if (id_user) {
+        let essai = await EssaiModel.find({
+            $or: [
+                { id_user: id_user },
+            ]
+        })
+            .catch((error) => {
+                console.log(error);
+            });
+        if (essai != null) {
+            res.send(essai);
+        } else {
+            let string = { error: "Vous n'avez enregister aucuns essais clinique!" };
+            res.send(string)
+        }
+    }
+})
+
+module.exports = router;
