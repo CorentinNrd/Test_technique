35d74fb5cef5b16f69a14e3da23aaed0f70994c8
CorentinNrd <corentin.nordmann@epitech.eu>
2022-03-28 12:56:11 +0200

    update - mise en page et en style de la page d'accueil

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,16 +1,55 @@
 <template>
   <div>
-    <Nav />
-    <div class="grid grid-cols-3 justify-items-center mt-10">
-      <ul v-for="infos in info" v-bind:key="infos.id" class="mb-7">
-        <li>ID : {{ infos.A }}</li>
-        <li>Spécialité : {{ infos.B }}</li>
-        <li>Organe : {{ infos.C }}</li>
-        <li>Situation : {{ infos.D }}</li>
-      </ul>
+    <div class="w-full h-[300px] bg-slate-500"></div>
+    <div class="grid grid-cols-2 ml-5">
+      <div class="mt-10">
+        <h1 class="text-lg mb-5 underline">Essais clinique</h1>
+        <div class="grid grid-cols-2 ">
+          <ul
+            v-for="infos in info"
+            v-bind:key="infos.id"
+            class="border rounded mr-3 mb-7"
+          >
+            <li class="ml-2 mt-2 mb-2">ID : {{ infos.A }}</li>
+            <li class="ml-2 mt-2 mb-2">Spécialité : {{ infos.B }}</li>
+            <li class="ml-2 mt-2 mb-2">Organe : {{ infos.C }}</li>
+            <li class="ml-2 mt-2 mb-2">Situation : {{ infos.D }}</li>
+          </ul>
+        </div>
+      </div>
+      <!-- ____________________________________ -->
+      <div class="flex flex-col mt-10 gap-7 mr-5 mb-10 ml-5">
+        <aside class="w-full">
+          <h1 class="text-lg mb-5 underline">Qui sommes nous ?</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eros
+            quam, pellentesque vitae ex vulputate, vestibulum rutrum erat.
+            Praesent tincidunt lorem vitae lectus dignissim, vitae pretium ante
+            vulputate. Nam nunc nisi, interdum vel blandit ac, aliquam id ex.
+            Nulla nec blandit libero. Proin dictum massa imperdiet arcu dictum,
+            placerat mattis tellus blandit. Donec ultricies ac ante non mattis.
+            Sed ac ante rutrum, eleifend justo nec, accumsan magna. Integer at
+            magna ac mi venenatis rhoncus.
+          </p>
+        </aside>
+        <section class="w-full">
+          <h1 class="text-lg mb-5 underline">Nos services</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eros
+            quam, pellentesque vitae ex vulputate, vestibulum rutrum erat.
+            Praesent tincidunt lorem vitae lectus dignissim, vitae pretium ante
+            vulputate. Nam nunc nisi, interdum vel blandit ac, aliquam id ex.
+            Nulla nec blandit libero. Proin dictum massa imperdiet arcu dictum,
+            placerat mattis tellus blandit. Donec ultricies ac ante non mattis.
+            Sed ac ante rutrum, eleifend justo nec, accumsan magna. Integer at
+            magna ac mi venenatis rhoncus.
+          </p>
+        </section>
+      </div>
     </div>
   </div>
 </template>
+
 
 <script>
 import { ref, onMounted } from "vue";
