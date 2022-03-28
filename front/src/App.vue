2e39df26da54623cdc77b2c16ca2184ee70df136
CorentinNrd <corentin.nordmann@epitech.eu>
2022-03-28 12:56:39 +0200

    update - ajout du footer sur l'App

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Nav />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Nav from "./components/Nav.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
-    Nav
+    Nav,
+    Footer,
   },
 };
 </script>
